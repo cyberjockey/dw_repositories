f5c3e3151bdb6f807de191f8e6232a0d24cdf3ea
cyberjockey.indra@gmail.com <cyberjockey.indra@gmail.com>
2022-11-29 06:57:24 +0000

    ok

--- models/example/test_DAG.sql
+++ models/example/test_DAG.sql
@@ -1,9 +1,15 @@
-with test_DAG as (
-    select * from {{ source('normalized_data_swavibr', 'accounts') }}
+with test_DAG1 as (
+    select * from {{ source('normalized_data_swavibr_accounts', 'accounts') }}
 ),
 
+with test_DAG2 as (
+    select * from {{ source('normalized_data_swavibr_actions_source','actions_source')}}
+)
+
 final as (
-    select * from test_DAG
+    select * from test_DAG1
+    union all
+    select * from test_DAG2
 )
 
 select * from final
